6fa1d8555379da1b1c06c7176c9abb3ffee415d1
Hasan Abdelbaset Abdelrahman <hassanelfalt60@gmail.com>
2022-11-25 23:55:54 +0200

    Add files via upload

---
+++ A.1TextStyle/script.js
@@ -0,0 +1,37 @@
+
+function ChangeFont(font){
+
+    document.getElementById("PAR").style.fontFamily = font;
+}
+function ChangeAlign(align){
+
+    document.getElementById("PAR").style.textAlign = align;
+}
+function ChangeHeight(height){
+
+    document.getElementById("PAR").style.lineHeight = height;
+}
+function ChangeLSpace(space){
+
+    document.getElementById("PAR").style.letterSpacing = space;
+}
+function ChangeIndent(indent){
+
+    document.getElementById("PAR").style.textIndent = indent;
+}
+function ChangeTransform(transform){
+
+    document.getElementById("PAR").style.textTransform = transform;
+}
+function ChangeDecorate(decore){
+
+    document.getElementById("PAR").style.textDecoration = decore;
+}
+function ChangeBorder(border){
+
+    document.getElementById("PAR").style.border = border;
+}
+function ChangeBorderColor(borderColor){
+
+    document.getElementById("PAR").style.borderColor = borderColor;
+}
