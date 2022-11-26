c23ef909196065c7de6f26b6df3fd526050e961d
Hasan Abdelbaset Abdelrahman <hassanelfalt60@gmail.com>
2022-11-26 22:52:48 +0200

    Delete A.3CardMaker directory

--- A.3CardMaker/cardFinal.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("carMsg2").innerText = getCookie("cardMsg");
-document.getElementById("cardImg2").src = getCookie("cardImg");
-
-function closeWindow(){
-    window.close();
-}
